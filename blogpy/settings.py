f4c1d0655245c23b281d172c1e32b3cbdc368757
Sobhan Salimi <58114909+sbhan7@users.noreply.github.com>
2020-03-29 20:33:48 +0430

    Update settings.py
    modify database

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -74,14 +74,10 @@ WSGI_APPLICATION = 'blogpy.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
- 'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'blog',
-    'USER': 'root',
-    'PASSWORD': '',
-    'HOST': '/opt/lampp/var/mysql/mysql.sock',
-    'PORT': '3306'
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
